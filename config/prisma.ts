bffabab9068439cc8e42dbc36f791027adcc6712
Umar Farouk Ilyas <yasmarfaq@yahoo.com>
2025-12-31 18:43:37 +0100

    migrate to Supabase database

--- config/prisma.ts
+++ config/prisma.ts
@@ -2,19 +2,12 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// Creating Prisma client with specific configuration for PgBouncer
 const prisma = globalForPrisma.prisma || new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
   log: process.env.NODE_ENV === 'development' 
     ? ['query', 'error', 'warn'] 
     : ['error'],
 });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
 
 export default prisma;
